084a8d677e93fb9bcdbdbc39897358458a8fd940
Edmilson-Lopes <milsonlopes@hotmail.com>
2024-02-26 22:27:15 -0300

    Curso Rspec

---
+++ lib/base_sintaxe.rb
@@ -0,0 +1,12 @@
+
+require 'spec_helper'
+require 'hero'
+
+describe Hero do 
+    context 'quando está com armadura' do 
+        it 'tem 5000 pontos de HP' do 
+            hero = Hero.new 
+            expect(hero.hp).to eq(5000)
+        end
+    end
+end
